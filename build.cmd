220b2350f78d03efea4b64d8e14e0e60c51278b5
Kulishenko Igor <KulishenkoIV@oschadbank.ua>
2017-07-26 14:51:35 +0300

    changed build script.

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 @ echo off
 cls
 @ pushd src
-C:\Java\jdk1.8.0_77\bin\javac.exe  ./ua/oschadbank/sender/*.java  -d ../build -g  -Xdiags:verbose -deprecation
+C:\Java\jdk1.8.0_77\bin\javac.exe  ./ua/oschadbank/sender/*.java  -d ../build -g  -Xdiags:verbose -Xlint:unchecked -deprecation
 @ popd
---
+++ test.cmd
@@ -0,0 +1,5 @@
+@echo off
+@cls
+@ pushd build
+C:\Java\jdk1.8.0_77\bin\java.exe  ua.oschadbank.sender.TestSender
+@ popd
